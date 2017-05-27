7ba032d99c05b84abb9dddc14cb50d5db9f3a972
Mohamed Essam <mohamed.essam.arafa@gmail.com>
2017-05-27 15:16:52 +0200

    Migrated to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170526171256) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.string   "user_name"
     t.string   "email"
