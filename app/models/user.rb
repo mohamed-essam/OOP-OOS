64177909d517c4d43b7a991dcbc1bfc1a708eee1
Mohamed Essam <mohamed.essam.arafa@gmail.com>
2017-06-03 01:17:45 +0200

    Changed user errors order

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   has_many :orders
   has_many :reviews
+  validates :user_name, length: {minimum: 6, maximum: 32}, uniqueness: true
   validates :first_name, length: {minimum: 2, maximum: 32}
   validates :last_name, length: {minimum: 2, maximum: 32}
-  validates :email, format: {with: /(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})/i}, uniqueness: true
   validates :phone, presence: true
-  validates :user_name, length: {minimum: 6, maximum: 32}, uniqueness: true
+  validates :email, format: {with: /(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})/i}, uniqueness: true
   has_secure_password
 end
