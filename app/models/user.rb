9df659fb94354b51d3ab06a7ad3baf76b588521c
Mohamed Essam <mohamed.essam.arafa@gmail.com>
2017-06-03 01:27:11 +0200

    Added email format gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
   validates :first_name, length: {minimum: 2, maximum: 32}
   validates :last_name, length: {minimum: 2, maximum: 32}
   validates :phone, presence: true
-  validates :email, format: {with: /(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})/i}, uniqueness: true
+  validates_email_format_of :email, :message => 'is invalid'
   has_secure_password
 end
